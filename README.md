36404c468632d1e37e216f7350ec7a677a62bb4c
Shawon Lodh <shawonlodh2000@gmail.com>
2021-01-27 15:57:16 +0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Flash-Chat
+# Flash Chat ⚡️
+
+## What is Implemented?
+
+
+A modern messaging app where users can sign up and log in to chat. This apps is incorporated with Firebase.Also this app use Firebase Cloud Firestore as well as the Firebase authentication package to use a cloud-based NoSQL database and secure authentication methods.
+
+![Finished App](https://github.com/Shawon-Lodh/Bitcoin-Exchange/blob/main/bitcoin.gif)
+
+## Language
+- Dart
+## SDK
+- Flutter
+## Platform
+- Android,IOS
+
+## Strengths
+- Incorporate Firebase into your Flutter projects.
+- Using Firebase authentication to register and sign in users.
+- Using Firebase Cloud Firestore to store and retrieve data for real time chatting.
+- Creating beautiful animations using the Flutter Hero widget for transiting from one page to another page.
+- Creating text animation by animated-text kit package.
+- Creating custom aniamtions using Flutter's animation controller.
+- Creating bubble UI in every message and make differ sender and receiver message bubble UI.
+- Using Email Address based keyboard for suggestion of email domain.
+- Using obsecure text for making the text as a secret type in password text field.
+- Using mixins and differ from superclasses. 
+- Using Streams to load message from server.
+- Using ListViews to build scrolling views.
+- Multi-page apps using Flutter NamedRoutes and Navigator using static Modifier.
+- Extracting Widgets to refactor the code.
+- Using the private and public modifiers.
+- seperate the constant (varibale,widgets) class.
+- suitable use of final modifier.
+
+N.B : The IDE which is used to create this readme file is found [here](https://dillinger.io/).
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,97 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+
+import '../components/Rounded_button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static String id = 'welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+      // upperBound: 100,
+    );
+
+    animation =
+        ColorTween(begin: Colors.grey, end: Colors.white).animate(controller);
+    controller.forward();
+    controller.addListener(() {
+      // print(animation.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Flexible(
+                  child: Hero(
+                    tag: 'logo',
+                    child: Container(
+                      child: Image.asset('images/logo.png'),
+                      height: 60, //60.0,
+                    ),
+                  ),
+                ),
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            roundedButton(
+              buttonText: 'Log In',
+              colour: Colors.lightBlueAccent,
+              onPressed: () {
+                //Go to login screen.
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+            ),
+            roundedButton(
+              buttonText: 'Register',
+              colour: Colors.blueAccent,
+              onPressed: () {
+                //Go to login screen.
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
